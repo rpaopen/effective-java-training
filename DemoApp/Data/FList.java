a4216d3053c9748c9be612f04b619cc7e75fd117
rpaopen <73092889+rpaopen@users.noreply.github.com>
2020-10-19 16:57:27 +0530

    Add files via upload

---
+++ DemoApp/Data/FList.java
@@ -0,0 +1,17 @@
+Class Flist 
+{
+public static void maid (String [] args)
+
+{
+    FList ListFurnit= New FList();
+                  
+          
+}
+
+            int add()
+           {
+
+            }
+
+
+}
