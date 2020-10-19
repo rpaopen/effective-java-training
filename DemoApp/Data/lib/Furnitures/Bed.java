3d4a873105b6c65db8d57531df6b9fccb1c81424
rpaopen <73092889+rpaopen@users.noreply.github.com>
2020-10-19 16:59:45 +0530

    Add files via upload

---
+++ DemoApp/Data/lib/Furnitures/Bed.java
@@ -0,0 +1,10 @@
+Class Bed{
+
+Public static void main (string [] args){
+      int Beds =2
+      int Blankets =2 
+
+
+}
+
+}
---
+++ DemoApp/Data/lib/Furnitures/Chair.java
@@ -0,0 +1,12 @@
+Class Chair{
+
+Public satic void main (string [] args)
+{
+
+   int chairs =2;
+   String ChairColourOne ="Red"
+   String ChairColourTwo ="Blue"
+
+}
+
+}
