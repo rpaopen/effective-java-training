4fec4d051775c0cf5127df182a000252a97a1ed2
rpaopen <rpaopen@gmail.com>
2020-10-21 23:54:06 +0530

    Assignment07

---
+++ Assignment07/in.conceptarchitect.banking/src/in/conceptarchitect/banking/BankAccount.java
@@ -0,0 +1,29 @@
+package in.conceptarchitect.banking;
+
+import in.conceptarchitect.utils.Input;
+
+public class BankAccount {
+	
+	int accountNumber;
+	String name;
+	String password;
+	
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	
+	//you can't change your account number
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password=password;
+	}
+	
+	public int ATMPin() {
+		return 10;
+	
+	}
+
+}
